3e55450fa0580b81690d1e927fec705354f71630
prince vermandere <prince.vermandere@outlook.be>
2021-08-05 06:57:39 +0200

    Initial commit

---
+++ src/main/java/com/example/atbean/AtBeanApplication.java
@@ -0,0 +1,13 @@
+package com.example.atbean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtBeanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtBeanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/atbean/AtBeanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.atbean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtBeanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
